94c32b3dfca0626a17ef2d3688cb0e4c9554bf3d
karthikraja46 <96817647+karthikraja46@users.noreply.github.com>
2025-06-08 19:16:37 +0530

    Learning basic concepts of type script

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { Greet } from './components/Greet';
+import { Person } from './components/Person';
+import { PersonList } from './components/PersonList';
 function App() {
+  const personName = {
+    first: 'John',
+    last: 'Doe'
+  };
+
+  const nameList = [
+    {
+      first: 'Karthik',
+      last: 'Raja'
+    },
+    {
+      first: 'John',
+      last: 'Doe'
+    },
+    {
+      first: 'Jane',
+      last: 'Smith'
+    }
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Greet name="John" messageCount={20} isLoggedIn={false} />
+      <Person name={personName} />
+      <PersonList names={nameList} />
     </div>
   );
 }
---
+++ src/components/Greet.tsx
@@ -0,0 +1,23 @@
+// types are used when building applications
+// Interfaces are used when building libraries
+//Props is a special keyword in React that stands for properties and is used for passing data from one component to another
+type GreetProps = {
+    name: string;
+    messageCount: number;
+    isLoggedIn: boolean;
+}
+
+export const Greet = (props: GreetProps) => {
+  return (
+    <div>
+        <h1>
+            {
+                props.isLoggedIn ? `Welcome ${props.name}! You have ${props.messageCount} unread messages.` : 'Welcome Guest!'
+            }
+        </h1>
+      <p>Welcome to my React application.</p>
+    </div>
+  );
+}
+
+
